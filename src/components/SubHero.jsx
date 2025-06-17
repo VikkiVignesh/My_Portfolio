e9cd6c11f59a17c6fd1c2b7add80ad6487cce11a
VikkiVignesh <Vikki.Vishnu04@gmail.com>
2025-06-17 22:50:46 +0530

    Handled responsiveness

--- src/components/SubHero.jsx
+++ src/components/SubHero.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const SubHero = () => {
-  return (
-    <div className='w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase text-4xl md:text-3xl sm:text-2xl py-6 items-center gap-4 bg-brown'>
-      <p className='md:block sm:hidden'>Fast Learner</p>
-      <p className='md:block sm:hidden'>Team Player</p>
-      <p>Detail-Oriented</p>
+ return(
+   <div className='w-full border-y border-lightGrey text-lightGrey flex flex-wrap justify-center text-center uppercase text-4xl md:text-3xl sm:text-xl py-6 items-center gap-6 sm:gap-4 bg-brown'>
+      <p className='w-auto sm:w-full'>Fast Learner</p>
+      <p className='w-auto sm:w-full'>Team Player</p>
+      <p className='w-auto sm:w-full'>Detail-Oriented</p>
     </div>
-  );
+ );
+  
 };
 
 export default SubHero;
